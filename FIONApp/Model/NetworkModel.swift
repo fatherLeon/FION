ddee45857fd2eeced9c9650fc8384eb3d045d2bc
minsu3007 <minsu3007@gmail.com>
2023-04-01 17:34:21 +0900

    refactor: makeURLSessionDataTask Method

--- FIONApp/Model/NetworkModel.swift
+++ FIONApp/Model/NetworkModel.swift
@@ -7,49 +7,42 @@
 
 import Foundation
 
-class NetworkModel {
+struct NetworkModel {
+    let session: URLSession
+    
     func makeRequest(contentType: ContentType) -> URLRequest? {
         guard let url = contentType.url else { return nil }
         
         var request = URLRequest(url: url)
         
         request.httpMethod = "GET"
+        request.addValue("\(Bundle.main.apiKey)", forHTTPHeaderField: "Authorization")
         
         return request
     }
     
-    func fetchUserInfo(completion: @escaping (Result<Data, NetworkError>) -> Void) -> URLSessionDataTask {
-        do {
-            let url = try getUserInfoURL(items: [URLQueryItem(name: "nickname", value: nickName)])
-            var request = URLRequest(url: url)
-            
-            request.addValue("\(Bundle.main.apiKey)", forHTTPHeaderField: "Authorization")
-            
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                guard error == nil else {
-                    completion(.failure(.networkError))
-                    return
-                }
-                guard let httpResponse = response as? HTTPURLResponse,
-                      (200...299).contains(httpResponse.statusCode) else {
-                    completion(.failure(.responseError))
-                    return
-                }
-                
-                if let data = data {
-                    do {
-                        completion(.success(decodingData))
-                    } catch {
-                        completion(.failure(.decodingError))
-                        return
-                    }
-                }
+    func makeURLSessionDataTask(request: URLRequest, completion: @escaping ((Result<Data, NetworkError>) -> Void)) -> URLSessionDataTask {
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil,
+                  let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(.networkError))
+                return
             }
             
-            task.resume()
-        } catch {
-            completion(.failure(.invalidURL))
-            return
+            guard (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(.responseError(httpResponse.statusCode)))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.networkError))
+                return
+            }
+            
+            completion(.success(data))
         }
+        
+        return task
     }
 }
