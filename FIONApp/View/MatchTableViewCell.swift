329d96db5d48c16a83f796dca4d8b332e93ffc95
minsu3007 <minsu3007@gmail.com>
2023-04-06 22:07:49 +0900

    delete: myNicknameLabel

--- FIONApp/View/MatchTableViewCell.swift
+++ FIONApp/View/MatchTableViewCell.swift
@@ -10,17 +10,6 @@ import UIKit
 class MatchTableViewCell: UITableViewCell {
     
     static let identifier = "MatchTableViewCell"
-
-    private let myNicknameLabel = {
-        let label = UILabel()
-        
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.numberOfLines = 1
-        label.font = .preferredFont(forTextStyle: .body)
-        label.textAlignment = .center
-        
-        return label
-    }()
     
     private let enemyNicknameLabel = {
         let label = UILabel()
@@ -45,7 +34,7 @@ class MatchTableViewCell: UITableViewCell {
     }()
     
     private lazy var mainStackView = {
-        let stackView = UIStackView(arrangedSubviews: [myNicknameLabel, scoreLabel, enemyNicknameLabel])
+        let stackView = UIStackView(arrangedSubviews: [scoreLabel, enemyNicknameLabel])
         
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.spacing = 6
@@ -66,7 +55,6 @@ class MatchTableViewCell: UITableViewCell {
     
     func updateLabelText(_ data: MatchObject, target: String) {
         if data.matchInfo[0].nickname == target {
-            self.myNicknameLabel.text = data.matchInfo[0].nickname
             self.enemyNicknameLabel.text = data.matchInfo[1].nickname
             
             var scoreText = ""
@@ -76,7 +64,6 @@ class MatchTableViewCell: UITableViewCell {
             
             self.scoreLabel.text = scoreText
         } else {
-            self.myNicknameLabel.text = data.matchInfo[1].nickname
             self.enemyNicknameLabel.text = data.matchInfo[0].nickname
             
             var scoreText = ""
