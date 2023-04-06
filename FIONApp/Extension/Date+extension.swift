6fe800d8138ff80a1542eafed8b399e939d73488
minsu3007 <minsu3007@gmail.com>
2023-04-06 23:15:17 +0900

    feat: ISOFormatter & makeListDateFormatter

--- FIONApp/Extension/Date+extension.swift
+++ FIONApp/Extension/Date+extension.swift
@@ -8,10 +8,16 @@
 import Foundation
 
 extension Date {
-    static let matchDateFormatter = {
+    static let ISOFormatter = {
+        let dateFormatter = ISO8601DateFormatter()
+        
+        return dateFormatter
+    }()
+    
+    static let matchListDateFormatter = {
         let dateFormatter = DateFormatter()
         
-        dateFormatter.dateFormat = "MM-dd, HH:mm"
+        dateFormatter.dateFormat = "MM-dd HH:mm"
         
         return dateFormatter
     }()
