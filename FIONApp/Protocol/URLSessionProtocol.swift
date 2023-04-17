32c1f7fc716103fa0caefd6697913359eee917fd
minsu3007 <minsu3007@gmail.com>
2023-04-17 18:55:23 +0900

    feat: URLSessionProtocol

---
+++ FIONApp/Protocol/URLSessionProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  URLSessionProtocol.swift
+//  FIONApp
+//
+//  Created by 강민수 on 2023/04/17.
+//
+
+import Foundation
+
+protocol URLSessionProtocol {
+    func makeRequest(contentType: ContentType) -> URLRequest?
+    func makeURLSessionDataTask(request: URLRequest, completion: @escaping ((Result<Data, NetworkError>) -> Void)) -> URLSessionDataTask
+}
