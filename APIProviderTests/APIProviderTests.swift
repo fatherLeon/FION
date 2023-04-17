6f2c3c35297bfa0e1dead81aa4e9a3fad596cfd7
minsu3007 <minsu3007@gmail.com>
2023-04-17 19:42:29 +0900

    feat: test_validRequest

--- APIProviderTests/APIProviderTests.swift
+++ APIProviderTests/APIProviderTests.swift
@@ -6,12 +6,41 @@
 //
 
 import XCTest
+@testable import FIONApp
 
 final class APIProviderTests: XCTestCase {
+    
+    private var sut: APIProvider!
 
     override func setUpWithError() throws {
+        sut = APIProvider()
     }
 
     override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func test_validRequest() {
+        // given
+        let contentType = ContentType.match(matchid: "123")
+        let httpMethodExpectation = "GET"
+        let urlExpectation = contentType.url
+        
+        // when
+        let request = sut.makeRequest(contentType: contentType)
+        
+        // then
+        XCTAssertEqual(request?.httpMethod, httpMethodExpectation)
+        XCTAssertEqual(request?.url, urlExpectation)
+    }
+    
+    func test_validDataTask() {
+        // given
+        let contentType = ContentType.match(matchid: "123")
+        let request = sut.makeRequest(contentType: contentType)
+        
+        // when
+        
+        // then
     }
 }
---
+++ APIProviderTests/MockURLProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  MockURLProtocol.swift
+//  APIProviderTests
+//
+//  Created by 강민수 on 2023/04/17.
+//
+
+import Foundation
