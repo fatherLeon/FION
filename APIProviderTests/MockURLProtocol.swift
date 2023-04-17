324455fd55d2dd1f21da36b28f53b945f4858bad
minsu3007 <minsu3007@gmail.com>
2023-04-18 04:13:25 +0900

    feat: MockURLProtocol

--- APIProviderTests/MockURLProtocol.swift
+++ APIProviderTests/MockURLProtocol.swift
@@ -8,4 +8,19 @@
 import Foundation
 
 class MockURLProtocol: URLProtocol {
+    static var requestHandler: ((URLRequest) throws -> (URLResponse, Data))?
+    
+    override func startLoading() {
+        guard let handler = MockURLProtocol.requestHandler else { return }
+        
+        do {
+            let (response, data) = try handler(request)
+            
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
 }
