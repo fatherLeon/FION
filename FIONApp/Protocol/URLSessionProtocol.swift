d1f8d4d72d20f8a73224be46d587dae16c40dc35
minsu3007 <minsu3007@gmail.com>
2023-04-17 18:58:35 +0900

    delete: URLSessionProtocol-makeRequest

--- FIONApp/Protocol/URLSessionProtocol.swift
+++ FIONApp/Protocol/URLSessionProtocol.swift
@@ -8,6 +8,5 @@
 import Foundation
 
 protocol URLSessionProtocol {
-    func makeRequest(contentType: ContentType) -> URLRequest?
     func makeURLSessionDataTask(request: URLRequest, completion: @escaping ((Result<Data, NetworkError>) -> Void)) -> URLSessionDataTask?
 }
