2c84fe66c187636fd41842f74e894c4e35f1fa22
minsu3007 <minsu3007@gmail.com>
2023-04-17 19:30:52 +0900

    init: APIProviderTests

---
+++ APIProviderTests/APIProviderTests.swift
@@ -0,0 +1,17 @@
+//
+//  APIProviderTests.swift
+//  APIProviderTests
+//
+//  Created by 강민수 on 2023/04/17.
+//
+
+import XCTest
+
+final class APIProviderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
