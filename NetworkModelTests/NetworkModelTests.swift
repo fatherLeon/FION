a5a6f5e85a795fe4e61a329da4ac2768beefb20d
minsu3007 <minsu3007@gmail.com>
2023-04-17 18:47:46 +0900

    init: NetworkModelTests

---
+++ NetworkModelTests/NetworkModelTests.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkModelTests.swift
+//  NetworkModelTests
+//
+//  Created by 강민수 on 2023/04/17.
+//
+
+import XCTest
+
+final class NetworkModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
