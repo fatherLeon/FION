340267ccebcee09be83ccf29d5336977e8b7ca4d
minsu3007 <minsu3007@gmail.com>
2023-04-18 05:09:05 +0900

    feat: Decoder-decodeToImage method

--- FIONApp/Model/Network/Decoder.swift
+++ FIONApp/Model/Network/Decoder.swift
@@ -5,12 +5,18 @@
 //  Created by 강민수 on 2023/04/18.
 //
 
-import Foundation
+import UIKit
 
 struct Decoder<T> {
     func decodeToJson(by data: Data) -> T? where T: Decodable {
         let decodingData = try? JSONDecoder().decode(T.self, from: data)
         
         return decodingData
+    }
+    
+    func decodeToImage(by data: Data) -> UIImage? {
+        let decodingImage = UIImage(data: data)
+        
+        return decodingImage
     }
 }
