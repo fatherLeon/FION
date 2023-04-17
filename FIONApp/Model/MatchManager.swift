bdfd144dc917e8a700062c3392688ab8a00b7fad
minsu3007 <minsu3007@gmail.com>
2023-04-18 05:31:34 +0900

    fix: MatchManager

--- FIONApp/Model/MatchManager.swift
+++ FIONApp/Model/MatchManager.swift
@@ -9,14 +9,14 @@ import Foundation
 
 class MatchManager {
     private let matchId: [String]
-    private var networkManagers: [NetworkManager<MatchObject>] = []
+    private var networkManagers: [NetworkManager] = []
     var matchesInfo: [MatchObject] = []
     
     init(matchIds: [String]) {
         self.matchId = matchIds
         
         matchIds.forEach { id in
-            let manager = NetworkManager<MatchObject>(type: .match(matchid: id))
+            let manager = NetworkManager(type: .match(matchid: id))
             
             networkManagers.append(manager)
         }
@@ -24,7 +24,7 @@ class MatchManager {
     
     func fetchMatchInfo() {
         networkManagers.forEach { manager in
-            manager.fetchDataByJson { [weak self] result in
+            manager.fetchDataByJson(to: MatchObject.self) { [weak self] result in
                 switch result {
                 case .success(let data):
                     self?.matchesInfo.append(data)
