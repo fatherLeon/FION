3d4483e8eb99803bd43901567ade0e5fbd08386c
minsu3007 <minsu3007@gmail.com>
2023-04-19 00:17:56 +0900

    feat: fetchAllMatchData method

--- FIONApp/ViewController/MainVC/MainUIModel.swift
+++ FIONApp/ViewController/MainVC/MainUIModel.swift
@@ -13,4 +13,17 @@ final class MainUIModel {
     func fetchUserDataByJson<T>(manager: NetworkManager, _ type: T.Type, handler: @escaping (Result<T, NetworkError>) -> Void) where T: Decodable {
         manager.fetchDataByJson(to: type, handler: handler)
     }
+    
+    func fetchAllMatchData() {
+        let manager = NetworkManager(type: .allMatch())
+        
+        manager.fetchDataByJson(to: UserMatchObject.self) { [weak self] result in
+            switch result {
+            case .success(let data):
+                self?.fetchMatchDescData(data.matchIds)
+            case .failure(_):
+                return
+            }
+        }
+    }
 }
