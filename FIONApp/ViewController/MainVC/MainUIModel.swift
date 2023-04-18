ecb792b35bbd3c847d8ea413ebd102714f63dc93
minsu3007 <minsu3007@gmail.com>
2023-04-19 03:06:21 +0900

    feat: fetchPlayerImages method

--- FIONApp/ViewController/MainVC/MainUIModel.swift
+++ FIONApp/ViewController/MainVC/MainUIModel.swift
@@ -8,19 +8,25 @@
 import UIKit
 
 final class MainUIModel {
+    private let dataGroup = DispatchGroup()
+    private let imageGroup = DispatchGroup()
     private var playersCounter: [Int: Int] = [:]
-    private var playerImages: [UIImage] = []
-    private let group = DispatchGroup()
+    var playerImages: [UIImage] = []
     
     func fetchUserDataByJson<T>(manager: NetworkManager, _ type: T.Type, handler: @escaping (Result<T, NetworkError>) -> Void) where T: Decodable {
         manager.fetchDataByJson(to: type, handler: handler)
     }
     
-    func fetchPlayersImage(handler: @escaping () -> Void) {
+    func fetchPlayerImages(handler: @escaping () -> Void) {
         fetchAllMatchData()
-        fetchImages(handler: handler)
         
-        group.notify(queue: .global(), execute: handler)
+        dataGroup.notify(queue: .global()) { [weak self] in
+            self?.fetchImages()
+        }
+        
+        imageGroup.notify(queue: .global()) {
+            handler()
+        }
     }
     
     func fetchAllMatchData() {
@@ -36,16 +42,18 @@ final class MainUIModel {
         }
     }
     
-    func fetchMatchDescData(_ ids: [String]) {
+    private func fetchMatchDescData(_ ids: [String]) {
         ids.forEach { id in
+            dataGroup.enter()
             let networkModel = NetworkManager(type: .match(matchid: id))
             
             networkModel.fetchDataByJson(to: MatchObject.self) { [weak self] result in
                 switch result {
                 case .success(let data):
-                    let players = data.matchInfo[0].player + data.matchInfo[1].player
+                    let players = data.matchInfo[0].player
                     
                     self?.calculateUsedPlayer(players)
+                    self?.dataGroup.leave()
                 case .failure(_):
                     return
                 }
@@ -53,19 +61,21 @@ final class MainUIModel {
         }
     }
     
-    func fetchImages(handler: @escaping () -> Void) {
-        group.enter()
+    func fetchImages() {
         let players = calculateTopTenUsedPlayer()
         
         players.forEach { playerID in
+            imageGroup.enter()
+            
             let networkModel = NetworkManager(type: .actionImage(id: playerID))
             
             networkModel.fetchDataByImage { [weak self] result in
                 switch result {
                 case .success(let image):
                     guard let image = image else { return }
+                    
                     self?.playerImages.append(image)
-                    self?.group.leave()
+                    self?.imageGroup.leave()
                 case .failure(_):
                     return
                 }
