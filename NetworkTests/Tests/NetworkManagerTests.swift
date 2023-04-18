5cba7714739377915ee97153844fff93b987f3b3
minsu3007 <minsu3007@gmail.com>
2023-04-18 21:43:10 +0900

    init: NetworkManagerTests

---
+++ NetworkTests/Tests/NetworkManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManagerTests.swift
+//  NetworkTests
+//
+//  Created by 강민수 on 2023/04/18.
+//
+
+import XCTest
+
+final class NetworkManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
