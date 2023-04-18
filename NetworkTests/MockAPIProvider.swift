096dded7fc4a434669281b31e1a12e3c62c6433b
minsu3007 <minsu3007@gmail.com>
2023-04-18 22:04:20 +0900

    docs: seperate MockAPIProvider

---
+++ NetworkTests/MockAPIProvider.swift
@@ -0,0 +1,46 @@
+//
+//  MockAPIProvider.swift
+//  NetworkTests
+//
+//  Created by 강민수 on 2023/04/18.
+//
+
+import Foundation
+@testable import FIONApp
+
+struct MockAPIProvider: Providable {
+    
+    private let session: URLSession
+    
+    init() {
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [MockURLProtocol.self]
+        
+        let session = URLSession(configuration: configuration)
+        
+        self.session = session
+    }
+    
+    func makeRequest(contentType: ContentType) -> URLRequest? {
+        let request = URLRequest(url: contentType.url!)
+        
+        return request
+    }
+    
+    func makeURLSessionDataTask(request: URLRequest, completion: @escaping ((Result<Data, NetworkError>) -> Void)) -> URLSessionDataTask? {
+        
+        let task = session.dataTask(with: request) { data, response, _ in
+            guard let response = response as? HTTPURLResponse,
+                  (200...299).contains(response.statusCode) else {
+                completion(.failure(.responseError(400)))
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            completion(.success(data))
+        }
+        
+        return task
+    }
+}
--- NetworkTests/Tests/NetworkManagerTests.swift
+++ NetworkTests/Tests/NetworkManagerTests.swift
@@ -9,43 +9,6 @@ import XCTest
 @testable import FIONApp
 
 final class NetworkManagerTests: XCTestCase {
-    
-    struct MockAPIProvider: Providable {
-        
-        private let session: URLSession
-        
-        init() {
-            let configuration = URLSessionConfiguration.default
-            configuration.protocolClasses = [MockURLProtocol.self]
-            
-            let session = URLSession(configuration: configuration)
-            
-            self.session = session
-        }
-        
-        func makeRequest(contentType: ContentType) -> URLRequest? {
-            let request = URLRequest(url: contentType.url!)
-            
-            return request
-        }
-        
-        func makeURLSessionDataTask(request: URLRequest, completion: @escaping ((Result<Data, NetworkError>) -> Void)) -> URLSessionDataTask? {
-            
-            let task = session.dataTask(with: request) { data, response, _ in
-                guard let response = response as? HTTPURLResponse,
-                      (200...299).contains(response.statusCode) else {
-                    completion(.failure(.responseError(400)))
-                    return
-                }
-                
-                guard let data = data else { return }
-                
-                completion(.success(data))
-            }
-            
-            return task
-        }
-    }
 
     override func setUpWithError() throws {
     }
