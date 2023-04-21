6421c0f5885e7388527b286881f0dde1a04a517d
minsu3007 <minsu3007@gmail.com>
2023-04-21 21:32:26 +0900

    refactor: sepeartor case

--- FIONApp/Model/PlayerSection.swift
+++ FIONApp/Model/PlayerSection.swift
@@ -7,20 +7,26 @@
 
 enum PlayerSection: CaseIterable {
     case goalkeeper
-    case defender
+    case centerback
+    case sideback
     case midfielder
+    case winger
     case striker
     
     var positionNumber: [Int] {
         switch self {
         case .goalkeeper:
             return [0]
-        case .defender:
-            return Array(1...8)
+        case .centerback:
+            return [1, 4, 5, 6]
+        case .sideback:
+            return [2, 3, 7, 8]
         case .midfielder:
-            return Array(9...19)
+            return [9, 10, 11, 13, 14, 15, 16, 17, 18, 19]
+        case .winger:
+            return [12, 16, 23, 27]
         case .striker:
-            return Array(20...27)
+            return [20, 21, 22, 24, 25, 26, 27]
         }
     }
 }
