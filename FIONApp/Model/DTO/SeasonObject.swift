944c5a03d7ba5a5992266562e6ea915cd192adfd
minsu3007 <minsu3007@gmail.com>
2023-04-21 17:26:12 +0900

    feat: SeasonObject type

---
+++ FIONApp/Model/DTO/SeasonObject.swift
@@ -0,0 +1,14 @@
+//
+//  SeasonObject.swift
+//  FIONApp
+//
+//  Created by 강민수 on 2023/04/21.
+//
+
+import Foundation
+
+struct SeasonObject: Decodable {
+    let seasonId: Int
+    let className: String
+    let seasonImg: String
+}
